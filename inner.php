ece7e801b31f33565caed02d3d7f030dc2076835
jaychow <jay.chow@katanaconnect.com>
2015-12-07 22:47:09 -0800

    style colors

--- inner.php
+++ inner.php
@@ -11,7 +11,7 @@ include_once('include/header.php'); ?>
                     <li>
                         <p>YOU ARE HERE</p>
                     </li>
-                    <li><a href="#" class="active">themes</a></li>
+                    <li><a href="#" class="active">Inner page</a></li>
                 </ul>
                 <div class="page-title"><i class="icon-custom-left"></i>
 
@@ -27,24 +27,39 @@ include_once('include/header.php'); ?>
 
                                 <div style="margin-bottom:20px; height: 210px;">
                                     <a class="pull-left" style="padding-right: 10px;" href="#"><img
-                                            src="http://placehold.it/200" alt="illustration"></a>
+                                            src="http://placehold.it/400" alt="illustration"></a>
 
-                                    <p>is the art and technique of arranging type in order to make language visible.
-                                        The arrangement of type involves the selection of typefaces, point size, line
-                                        length,
-                                        leading (line spacing), adjusting the spaces between groups of letters
-                                        (tracking)
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+                                    </p>
+                                    <p>
+                                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh.
+                                    </p>
+                                    <p>
+                                        Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit.
+                                    </p>
+                                        Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi.
+                                    <p>
+                                        Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo. Sed pretium blandit orci. Ut eu diam at pede suscipit sodales.
                                     </p>
                                 </div>
+                                <div class="clearfix"></div>
                                 <div style="margin-bottom:20px">
                                     <a class="pull-right" href="#" style="padding-left: 10px;"><img
                                             src="http://placehold.it/200" alt="illustration"></a>
 
-                                    <p>is the art and technique of arranging type in order to make language visible.
-                                        The arrangement of type involves the selection of typefaces, point size, line
-                                        length,
-                                        leading (line spacing), adjusting the spaces between groups of letters
-                                        (tracking)
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+                                    </p>
+                                    <p>
+                                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh.
+                                    </p>
+                                    <p>
+                                        Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit.
+                                    </p>
+                                    Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi.
+                                    <p>
+                                        Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo. Sed pretium blandit orci. Ut eu diam at pede suscipit sodales.
                                     </p>
                                 </div>
                             </div>
@@ -65,7 +80,7 @@ include_once('include/header.php'); ?>
                                             <h4 class="panel-title">
                                                 <a data-toggle="collapse"
                                                    href="#collapse1">
-                                                    Collapsible Group 1</a>
+                                                    Section 1</a>
                                             </h4>
                                         </div>
                                         <div id="collapse1" class="panel-collapse collapse in">
@@ -84,7 +99,7 @@ include_once('include/header.php'); ?>
                                             <h4 class="panel-title">
                                                 <a data-toggle="collapse"
                                                    href="#collapse2">
-                                                    Collapsible Group 2</a>
+                                                    Section 2</a>
                                             </h4>
                                         </div>
                                         <div id="collapse2" class="panel-collapse collapse">
@@ -103,7 +118,7 @@ include_once('include/header.php'); ?>
                                             <h4 class="panel-title">
                                                 <a data-toggle="collapse"
                                                    href="#collapse3">
-                                                    Collapsible Group 3</a>
+                                                    Section 3</a>
                                             </h4>
                                         </div>
                                         <div id="collapse3" class="panel-collapse collapse">
